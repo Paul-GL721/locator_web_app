d054f8ad7018e5668c55c5161e37768bc9822aa4
server2 <lwangapaul23@gmail.com>
2025-04-16 13:44:11 +0300

    feat(models): add backend models
    add models to store data passed by the mobile applications

--- locator/models.py
+++ locator/models.py
@@ -1,3 +1,113 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+# manager for the custom employee abstract model
+
+
+class LocAppUserManager(BaseUserManager):
+    def create_user(self, telephone_Number, password=None,):
+        """
+        Creates and saves a User with a telephone_Number and password.
+        """
+        if not telephone_Number:
+            raise ValueError('Users must have a username')
+
+        locuser = self.model(
+            telephone_Number=telephone_Number,
+        )
+
+        locuser.set_password(password)
+        locuser.save(using=self._db)
+        return locuser
+
+    def create_superuser(self, telephone_Number, password=None):
+        """
+        Creates and saves a superuser.
+        """
+        superuser = self.model(
+            telephone_Number=telephone_Number,
+            password=password,
+        )
+
+        superuser.is_admin = True
+        superuser.save(using=self._db)
+        return superuser
+
+# Inherits from the default user model
+
+
+class LocAppUser(AbstractUser):
+    locuser_id = models.AutoField(primary_key=True)
+    telephone_Number = models.CharField(
+        max_length=200, unique=True, help_text='Contact Number of the employee')
+    Last_modified = models.DateTimeField(blank=True, null=True, auto_now=True)
+
+    objects = LocAppUserManager()
+    USERNAME_FIELD = "telephone_Number"
+
+    def __str__(self):
+        return f'{self.first_name}, {self.last_name}'
+
+
+# model to store locations
+class LocAppPositions(models.Model):
+    # fields
+    LocAppPos_id = models.AutoField(primary_key=True)
+    LocAppPos_Date = models.DateField(
+        help_text='Date the position was captured')
+    LocAppPos_user = models.ForeignKey(
+        'LocAppUser', on_delete=models.CASCADE, related_name='lociposition')
+    LocAppPos_timestamp = models.TimeField(
+        help_text='Time the reading was taken in HH:MM:SS')
+    LocAppPos_latitude = models.FloatField(
+        default=0, help_text='Latitude in decimal degrees from GPS')
+    LocAppPos_longitude = models.FloatField(
+        default=0, help_text='Longitude in decimal degrees captured from GPS')
+    LocAppPos_accuracy = models.FloatField(
+        default=0, help_text='Accuracy of latitude and Longitude in metres')
+    server_Captured_on = models.DateTimeField(
+        blank=False, auto_now_add=True, help_text='Date and time data was input in the server')
+    Last_modified = models.DateTimeField(
+        blank=False, auto_now=True, help_text='Date and time data was last updated in the server')
+    offline_Captured_on = models.DateTimeField(
+        blank=True, null=True, help_text='Date and Time the data was locally captured')
+    offline_pkid = models.CharField(
+        blank=True, null=True, max_length=20, help_text='Offline primary_key')
+
+    # Meta
+    class Meta:
+        ordering = ['LocAppPos_Date']
+
+    # Methods
+    def __str__(self):
+        return str(self.LocAppPos_id)
+
+    def get_absolute_url(self):
+        return reverse('position-detail', args=[str(self.LocAppPos_id)])
+
+# model to store LocAppgroups of user, who share the same tracking poistions
+
+
+class LocAppGroups(models.Model):
+    # Fields
+    LocAppGrp_id = models.AutoField(primary_key=True)
+    LocAppGrp_name = models.CharField(
+        blank=False, max_length=200, help_text='Enter Group name')
+    LocAppGrp_address = models.TextField(
+        blank=True, null=True, max_length=60, help_text='Enter Group address')
+    LocAppGrp_user = models.ManyToManyField(
+        'LocAppUser', related_name='locigroup')
+    LocAppGrp_description = models.TextField(
+        blank=True, null=True, max_length=150, help_text='Decribe your company')
+    server_Caputured_on = models.DateTimeField(
+        blank=False, auto_now_add=True, help_text='Date and time data was input in the server')
+    Last_modified = models.DateTimeField(blank=True, auto_now=True)
+
+    # Meta
+    class Meta:
+        ordering = ['LocAppGrp_name']
+
+    # Methods
+    def __str__(self):
+        return f'{self.LocAppGrp_name}'
