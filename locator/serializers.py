ed4abb17ef8291c43e368bb8a6b958148f6ecdbc
server2 <lwangapaul23@gmail.com>
2025-04-16 18:05:12 +0300

    refactor(api): create APIs
    create API that will allow mobile app communication with backend

---
+++ locator/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from .models import *
+
+
+# serializer for the app-user model
+class LocAppUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LocAppUser
+        fields = ['locuser_id', 'telephone_Number', 'Last_modified',]
+
+
+# serializer for the app-user-group model
+class LocAppGroupsSerializer(serializers.ModelSerializer):
+    LocAppGrp_user = LocAppUserSerializer(many=True)
+
+    class Meta:
+        model = LocAppUser
+        fields = ['LocAppGrp_id', 'LocAppGrp_name', 'LocAppGrp_address',
+                  'LocAppGrp_description', 'server_Captured_on', 'Last_modified', 'LocAppGrp_user']
+
+
+# serializer for the position model
+class LocAppPositionsSerializer(serializers.ModelSerializer):
+    LocAppPos_user = LocAppUserSerializer()
+
+    class Meta:
+        model = LocAppPositions
+        fields = ['LocAppPos_id', 'LocAppPos_Date', 'LocAppPos_timestamp', 'LocAppPos_latitude', 'LocAppPos_longitude',  'LocAppPos_accuracy',
+                  'server_Captured_on', 'Last_modified', 'offline_Captured_on', 'offline_pkid', 'LocAppPos_user']
