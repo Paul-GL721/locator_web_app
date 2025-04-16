77af65dc09a502c7d95676e321129d53af804caf
server2 <lwangapaul23@gmail.com>
2025-04-16 22:34:58 +0300

    feat(urls): add urls for site navigation
    add url to allow for user navigation

---
+++ locator/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    # web urls
+    # path(" ", index_page.as_view(), name="index_page"),
+    # path("afterlogin/", after_login.as_view(), name="after_login"),
+    path("generate-qr/", GenerateQRCodeView.as_view(), name="generate_qr"),
+    path("open-app/", OpenAppRedirectView.as_view(), name="open_app_redirect"),
+    path("generate-qr/", QRLoginGenerateView.as_view(), name="generate_qr"),
+    path("check-qr-login-status/<str:session_token>/",
+         check_qr_login_status, name="qr_login_check"),
+    path("api/qr-login-authenticate/",
+         QRLoginAuthenticateView.as_view(), name="qr_authenticate"),
+
+    # mobile urls
+    path('mobile/newgpsdata/', mobile_add_newgpsdata.as_view(),
+         name='add_newgpsdata'),
+
+    # mobile authentication
+    path('api-token-auth/', CustomAuthToken.as_view(), name='api_token_auth'),
+]
