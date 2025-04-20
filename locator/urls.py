38a62e3e1fcfdb71d529edff81df3da4ca45e7ef
server2 <lwangapaul23@gmail.com>
2025-04-20 12:18:20 +0300

    feat(urls): reroute traffic
    reroute traffic to the set url

--- locator/urls.py
+++ locator/urls.py
@@ -4,21 +4,20 @@ from .views import *
 urlpatterns = [
     # web urls
     path('', index_page.as_view(), name="index_page"),
-    # path("afterlogin/", after_login.as_view(), name="after_login"),
-    path("generate-qr/", GenerateQRCodeView.as_view(), name="generate_qr"),
-    path("open-app/", OpenAppRedirectView.as_view(), name="open_app_redirect"),
-    path("generate-qr/", QRLoginGenerateView.as_view(), name="generate_qr"),
-    path("check-qr-login-status/<str:session_token>/",
-         check_qr_login_status, name="qr_login_check"),
-    path("api/qr-login-authenticate/",
-         QRLoginAuthenticateView.as_view(), name="qr_authenticate"),
+    path('locator/', index_page.as_view(), name="index_page_indexed"),
+    path("afterlogin/", after_login.as_view(), name="after_login"),
+    path("adduser/", add_user.as_view(), name="add_user"),
+    # path("generate-qr/", GenerateQRCodeView.as_view(), name="generate_qr"),
+    # path("open-app/", OpenAppRedirectView.as_view(), name="open_app_redirect"),
+    # path("login-generate-qr/", QRLoginGenerateView.as_view(), name="generate_qr"),
+    # path("check-qr-login-status/<str:session_token>/", check_qr_login_status, name="qr_login_check"),
+    # path("api/qr-login-authenticate/",QRLoginAuthenticateView.as_view(), name="qr_authenticate"),
 
 
     # mobile urls
-    path('mobile/newgpsdata/', mobile_add_newgpsdata.as_view(),
-         name='add_newgpsdata'),
+    # path('mobile/newgpsdata/', mobile_add_newgpsdata.as_view(),name='add_newgpsdata'),
 
 
     # mobile authentication
-    path('api-token-auth/', CustomAuthToken.as_view(), name='api_token_auth'),
+    # path('api-token-auth/', CustomAuthToken.as_view(), name='api_token_auth'),
 ]
--- track_locator/urls.py
+++ track_locator/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.conf import settings
+from django.views.generic import RedirectView
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authentication/', include('django.contrib.auth.urls')),
-    path('', include('locator.urls')),
+    path('', RedirectView.as_view(url='locator/', permanent=True)),
+    path('locator/', include('locator.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
