2c5a2309a15aceb10481d226a045981cd351e379
server2 <lwangapaul23@gmail.com>
2025-04-21 22:12:52 +0300

    ci(pipeline): merge and build docker image
    merge and build image from the pipeline

---
+++ jenkins-scripts/build-step.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#### CHECK VERSIONS OF DOCKER AND COMPOSE ###
+docker-compose --version
+docker version
+echo "Building the docker staging image"
+
+
+# Build, tag, and push the staging image to docker public repository
+docker build -t ${DOCKER_ACCOUNT}/${REMOTE_REPO_NAME}:V$VERSION -f ./$BASE_DIRECTORY/Dockerfile ./$BASE_DIRECTORY
+
+whoami
+echo usr=$USER
+
+# Switch user and login and push image to docker hub 
+#(credentials are in the pass credsStore) 
+sudo su ubuntu <<HERE
+whoami
+echo usr=$USER
+docker push ${DOCKER_ACCOUNT}/${REMOTE_REPO_NAME}:V$VERSION
+HERE
+
+echo "Docker image pushed successfully to Docker Registry!"
---
+++ jenkins-scripts/merge-code-step.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+###Script to merge devlopment branch code into the staging branch###
+
+#set git username and email
+git config --global user.name "server2"
+git config --global user.email lwangapaul23@gmail.com
+echo The branches are:
+git branch
+git checkout -f production
+
+#If any of these files or folder changes in the development branch
+#merge them into the staging branch
+
+# Define the list of Django apps
+APPS=("locator")
+
+# Define the files to include for each app
+FILES=("admin.py" "apps.py" "forms.py" "models.py" "tests.py" "urls.py" "views.py")
+
+# Checkout selected files in a loop
+for app in "${APPS[@]}"; do
+    for file in "${FILES[@]}"; do
+        if [ -f "$BASE_DIRECTORY/$app/$file" ]; then
+            echo "Checking out: $BASE_DIRECTORY/$app/$file"
+            git checkout origin/staging "$BASE_DIRECTORY/$app/$file"
+        else
+            echo "Skipping: $BASE_DIRECTORY/$app/$file (file does not exist)"
+        fi
+    done
+done
+
+
+# Checkout additional necessary files
+git checkout origin/staging $BASE_DIRECTORY/track_locator $BASE_DIRECTORY/.gitignore \
+    $BASE_DIRECTORY/Dockerfile $BASE_DIRECTORY/entrypoint.sh $BASE_DIRECTORY/Jenkinsfile \
+    $BASE_DIRECTORY/manage.py $BASE_DIRECTORY/Pipfile $BASE_DIRECTORY/Pipfile.lock \
+    $BASE_DIRECTORY/README.md $BASE_DIRECTORY/templates $BASE_DIRECTORY/static 
+
+git status
+#git remote -v
+
+ 
